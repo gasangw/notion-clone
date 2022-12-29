61f82d600565c3cbf73ff6d1e8abc551cf1ee664
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-12-29 08:52:52 +0200

    Set-up react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import ChooseOption from './components/mainPage';
+// import Blocks from './components/blocks';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello react</p>
+    </div>
+  );
+}
+
+export default App;
