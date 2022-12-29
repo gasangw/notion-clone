70f1657f01e52814abf7f7a244f6e4d53a3b6988
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-12-29 15:31:17 +0200

    Add heading

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 // import { Routes, Route } from 'react-router-dom';
 // import ChooseOption from './components/mainPage';
 // import Blocks from './components/blocks';
+import Display from './components/display';
 
 function App() {
   return (
     <div className="App">
+      <Display />
       {/* <Routes>
         <Route path="/" element={<ChooseOption />} />
       </Routes> */}
--- src/components/display.js
+++ src/components/display.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsList } from 'react-icons/bs';
+// import ChooseOption from './mainPage';
 
-const Display = () => (
-  <div className="container">
-    <p>display</p>
-    <div>
-      <input
-        type="search"
-        className="input--section"
-        placeholder="Heading 1"
-      />
+const Display = () => {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
+
+  // create new heading
+  const addHeading = (heading) => {
+    const newHeading = {
+      id: Math.random(),
+      heading,
+    };
+
+    // add heading to the list
+    setList([...list, newHeading]);
+
+    // clear input
+    setInput('');
+  };
+
+  const handler = (e) => {
+    if (e.key === 'Enter') {
+      addHeading();
+    }
+  };
+  return (
+    <div className="wrapper">
+      <ul className="text-wrap">
+        {list.map((heading) => (
+          <li key={heading.id}>
+            <h1>{heading.heading}</h1>
+            <BsList />
+          </li>
+        ))}
+
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyPress={(e) => handler(e)}
+          className="type-field"
+          placeholder="Heading 1"
+        />
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Display;
