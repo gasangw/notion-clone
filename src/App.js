c14e47e8f3258853b360da5cc63be2b96409d010
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-12-30 11:02:09 +0200

    Update Readme file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-// import { Routes, Route } from 'react-router-dom';
-// import ChooseOption from './components/mainPage';
 import Main from './components/Main';
-// import Display from './components/display';
 
 function App() {
   return (
     <div className="App">
       <Main />
-      {/* <ChooseOption /> */}
-      {/* <Routes>
-        <Route path="/" element={<ChooseOption />} />
-      </Routes> */}
     </div>
   );
 }
